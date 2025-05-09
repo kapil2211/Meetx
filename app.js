4f37788fe98e2d5ba2a6d4b673d31d3711fceff7
iamkapil <kapilrhngdle2211@gmail.com>
2025-05-09 19:58:28 +0530

    routes cahnges

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/activities", require("./routes/activityRoutes"));
 app.use("/api/bookings", require("./routes/bookingRoutes"));
-
+app.get('/', (req, res) => {
+    res.send('Welcome to the Meetx API!');
+  });
+  
 app.listen(process.env.PORT, () =>
     console.log(`Server running on port ${process.env.PORT}`)
 );
